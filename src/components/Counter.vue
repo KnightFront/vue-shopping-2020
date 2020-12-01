6ffd769caf52c14db9e45a98b657c71fb5f46790
MrZhengXU <987078883@qq.com>
2020-12-01 23:27:55 +0800

    vue初版文件第一次提交

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <p>{{num}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:["num"],
+    data() {
+      return {
+        num:0
+      }
+    },
+    methods: {
+      increment(){
+        this.num++
+      },
+      decrement(){
+        this.num--
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
