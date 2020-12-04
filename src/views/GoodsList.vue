88cc027d2fb32923675c15b3d9d3c3bbba91bced
MrZhengXU <987078883@qq.com>
2020-12-04 23:05:58 +0800

    demo样例提交

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      这是商品列表页面
+      {{$route.params.goodsId}}
+       {{$route.params.name}}
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
